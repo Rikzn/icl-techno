5fff550ce0cfacc322c6093ee82bb3d2e2a3087a
Artem Safin <artemsafin.webdev@gmail.com>
2019-08-20 01:20:40 +0300

    Поправлены ховеры

--- src/js/tabs.js
+++ src/js/tabs.js
@@ -6,42 +6,41 @@ export default function() {
         const tabItems = Array.from(item.querySelector('.js-tabs-list').children);
         let activeTabIndex;
 
-        if (tabButtons && tabItems) {
-            if (tabButtons.length !== tabItems.length) {
-                console.log('Количество элементов управления не равно количеству табов');
-                return;
-            }
-            activeTabIndex = tabButtons.findIndex(element => {
-                return element.classList.contains('active');
-            });
-            if (activeTabIndex != -1) {
-                activeTabIndex = 0;
-            }
-
-            setActiveTab(activeTabIndex);
-
-            function setActiveTab(nextIndex, previousIndex) {
-                if (arguments.length >= 2) {
-                    tabButtons[previousIndex].classList.remove('active');
-                    tabItems[previousIndex].classList.remove('active');
-                }
-                tabButtons[nextIndex].classList.add('active');
-                tabItems[nextIndex].classList.add('active');
-                activeTabIndex = nextIndex;
-            }
-
-            tabButtons.forEach((button, index) => {
-                button.addEventListener('click', function(event) {
-                    event.preventDefault();
-                    if (index === activeTabIndex) {
-                        return;
-                    }
-                    setActiveTab(index, activeTabIndex);
-                })
-            });
-        } else {
+        if (!tabButtons.legth === 0 || tabItems.length === 0) {
             console.log('Отсутвуют кнопки или табы');
             return;
+        } else if (tabButtons.length !== tabItems.length) {
+            console.log('Количество элементов управления не равно количеству табов');
+            return;
         }
+        
+        activeTabIndex = tabButtons.findIndex(element => {
+            return element.classList.contains('active');
+        });
+        if (activeTabIndex != -1) {
+            activeTabIndex = 0;
+        }
+
+        setActiveTab(activeTabIndex);
+
+        function setActiveTab(nextIndex, previousIndex) {
+            if (arguments.length >= 2) {
+                tabButtons[previousIndex].classList.remove('active');
+                tabItems[previousIndex].classList.remove('active');
+            }
+            tabButtons[nextIndex].classList.add('active');
+            tabItems[nextIndex].classList.add('active');
+            activeTabIndex = nextIndex;
+        }
+
+        tabButtons.forEach((button, index) => {
+            button.addEventListener('click', function(event) {
+                event.preventDefault();
+                if (index === activeTabIndex) {
+                    return;
+                }
+                setActiveTab(index, activeTabIndex);
+            });
+        });
     });
 }
