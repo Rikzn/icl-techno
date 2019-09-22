7addb4d7a6d388cf0cd4b4eeedcd8a1711b295ae
Артем Сафин <artemsafin.webdev@gmail.com>
2019-09-22 10:24:44 +0300

    Слайдер Наша история подготовлен к натяжке

--- src/js/ourHistory.js
+++ src/js/ourHistory.js
@@ -31,9 +31,20 @@ export default function() {
             thumbs: {}
         };
 
-        if (historySliderThumbs) {
+        const historySliderThumbnailsToMove = Array.from(slider.querySelectorAll('.our-history__hidden-thumb-container .our-history__range-slider-block'));
+
+        if (historySliderThumbs && historySliderThumbnailsToMove.length !== 0) {
             const containerThumbs = historySliderThumbs.querySelector('.swiper-container');
             const wrapperThumbs = historySliderThumbs.querySelector('.swiper-wrapper');
+
+
+            historySliderThumbnailsToMove.forEach(thumbCard => {
+                const swiperSlide = document.createElement('div');
+                swiperSlide.className = 'swiper-slide';
+                swiperSlide.appendChild(thumbCard);
+                thumbCard.parentElement.remove();
+                wrapperThumbs.appendChild(swiperSlide);
+            })
 
             sliderOptions.thumbs.swiper = new Swiper(containerThumbs, {
                 slidesPerView: 'auto',
