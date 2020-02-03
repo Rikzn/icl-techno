d23017108495c9a7f8dcef790df0b614b50e7642
Артем Сафин <artemsafin.webdev@gmail.com>
2020-02-04 01:18:59 +0300

    Последняя страница конфигуратора

--- src/js/configurator.js
+++ src/js/configurator.js
@@ -1,32 +1,30 @@
 import PerfectScrollbar from 'perfect-scrollbar';
-
+import detectIt from 'detect-it';
 
 export default function() {
     const addToCartCloseBtns = Array.from(document.querySelectorAll('.js-add-to-card-close'));
 
     addToCartCloseBtns.forEach(btn => {
         btn.addEventListener('click', function(event) {
-            console.log('Clicked')
+            console.log('Clicked');
             event.preventDefault();
             const modal = this.closest('.js-added-to-cart-popover');
-            console.log(modal)
+            console.log(modal);
             if (modal) {
                 modal.classList.remove('configurator__added-to-cart--shown');
             }
-        })
-    })
-
-
+        });
+    });
 
     const scrollableTables = Array.from(document.querySelectorAll('.js-configurator-scrollable-table'));
 
-    if (document.body.classList.contains('is-touch')) return;
-
-    scrollableTables.forEach(table => {
-        const ps = new PerfectScrollbar(table, {
-            minScrollbarLength: 112,
-            maxScrollbarLength: 112,
-            wheelPropagation: false
+    if (!detectIt.hasTouch) {
+        scrollableTables.forEach(table => {
+            const ps = new PerfectScrollbar(table, {
+                minScrollbarLength: 112,
+                maxScrollbarLength: 112,
+                wheelPropagation: false
+            });
         });
-    })
+    }
 }
