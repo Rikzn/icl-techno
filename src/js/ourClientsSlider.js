be4f09a9d23eea7055ca800fc799df6206712b81
Artem Safin <artemsafin.webdev@gmail.com>
2020-03-23 14:43:19 +0300

    Фиксы

--- src/js/ourClientsSlider.js
+++ src/js/ourClientsSlider.js
@@ -1,18 +1,19 @@
 import Swiper from 'swiper';
 
-
 export default function() {
-    const slider = document.querySelector('.js-our-clients-slider');
+    if (document.body.classList.contains('is-admin')) return;
 
-    if (!slider || document.body.classList.contains('is-admin')) return;
+    const sliders = Array.from(document.querySelectorAll('.js-our-clients-slider'));
 
-    new Swiper(slider.querySelector('.swiper-container'), {
-        slidesPerView: 1,
-        effect: 'fade',
-        loop: true,
-        navigation: {
-            nextEl: slider.querySelector('.js-our-clients-slider-next'),
-            prevEl: slider.querySelector('.js-our-clients-slider-prev')
-        }
+    sliders.forEach(slider => {
+        new Swiper(slider.querySelector('.swiper-container'), {
+            slidesPerView: 1,
+            effect: 'fade',
+            loop: true,
+            navigation: {
+                nextEl: slider.querySelector('.js-our-clients-slider-next'),
+                prevEl: slider.querySelector('.js-our-clients-slider-prev')
+            }
+        });
     });
 }
