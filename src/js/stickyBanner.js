e50378134fe2a2d41c5d85e935c4144553b969c3
Artem Safin <artemsafin.webdev@gmail.com>
2020-04-04 02:20:15 +0300

    Сделано залипание баннера

--- src/js/stickyBanner.js
+++ src/js/stickyBanner.js
@@ -1,6 +1,31 @@
 
-import Sticky from 'sticky-js';
+// import Sticky from 'sticky-js';
+
+import ScrollMagic from 'ScrollMagic';
 
 export default function() {
-    new Sticky('.js-sticky-panel');
+    // new Sticky('.js-sticky-panel');
+
+
+    const stickyPanels = Array.from(document.querySelectorAll('.js-sticky-panel'));
+    
+    const pageFooter = document.querySelector('.page-footer');
+
+    if (!pageFooter) return;
+
+    const controller = new ScrollMagic.Controller();
+
+    stickyPanels.forEach(element => {
+        new ScrollMagic.Scene({
+            triggerElement: element,
+            triggerHook: 0.18,
+            duration: pageFooter.offsetTop
+        })
+            .setPin(element, {
+                pushFollowers: false
+            })
+            .setClassToggle(element, 'sticky')
+            .addTo(controller);
+    })
+
 }
