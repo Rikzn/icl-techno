6419d97aa9108cf8bb67c2557247c96e9b2cf899
Rikzn <41932726+Rikzn@users.noreply.github.com>
2021-03-02 19:03:29 +0300

    js-hover-a

---
+++ src/js/sliderProduct.js
@@ -0,0 +1,28 @@
+import Swiper from 'swiper';
+
+export default function sliderProject() {
+    const completedProjects = document.querySelectorAll('.product-block');
+
+    if (window.matchMedia('(max-width:700px)').matches) {
+        if (completedProjects) {
+
+            const swiper = new Swiper('.swiper-container', {
+                
+                direction: 'horizontal',
+                slidesPerView: 1,
+                loop: true,
+              
+                
+                pagination: {
+                  el: '.swiper-pagination',
+                },
+              
+              });
+        }
+    }
+
+    
+
+
+
+}
