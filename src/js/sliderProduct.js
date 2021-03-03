b6467203d830608b7cb2e9efbc3232c38fdf2874
Rikzn <41932726+Rikzn@users.noreply.github.com>
2021-03-03 15:32:14 +0300

    AOS-off

--- src/js/sliderProduct.js
+++ src/js/sliderProduct.js
@@ -8,17 +8,11 @@ export default function sliderProduct() {
             const mySwiper = new Swiper('.product-block-slider', {
                 loop: true,
               
-                // If we need pagination
                 pagination: {
                   el: '.swiper-pagination',
                   clickable: true,
                 },
-              
-                // Navigation arrows
-                navigation: {
-                  nextEl: '.swiper-button-next',
-                  prevEl: '.swiper-button-prev',
-                },
+
               })
         }
 }
