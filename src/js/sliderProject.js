66b3ed19e51a7c7a8374653aab9e8b0bab894b26
Rikzn <41932726+Rikzn@users.noreply.github.com>
2021-03-04 18:19:10 +0300

    slider+ button

--- src/js/sliderProject.js
+++ src/js/sliderProject.js
@@ -1,21 +1,15 @@
-import Swiper from 'swiper';
+import Swiper, { Navigation, Pagination } from 'swiper';
 
 export default function sliderProject() {
     const completedProjects = document.querySelectorAll('.completed-projects');
     if (completedProjects) {
-
-        const swiper = new Swiper('.swiper-container', {
+        Swiper.use([Navigation, Pagination]);
+        const mySwiper = new Swiper('.completed-projects-slider-container', {
             
             direction: 'horizontal',
             slidesPerView: 1,
             loop: true,
-          
-            
-            pagination: {
-              el: '.swiper-pagination',
-            },
-          
-            
+
             navigation: {
               nextEl: '.completed-projects__slider-next',
               prevEl: '.completed-projects__slider-prev',
