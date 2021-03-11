8acf2faa280aac13703f6cf20c303bb051edebfc
Rikzn <41932726+Rikzn@users.noreply.github.com>
2021-03-11 13:15:09 +0300

    top-popap

--- src/js/popap.js
+++ src/js/popap.js
@@ -8,8 +8,7 @@ export default function popap() {
         $(".modal").iziModal({
             width: 682,
             overlayColor: 'rgba(0, 0, 0, 0.8)',
-            transitionIn: 'fadeInUp',
-            transitionOut: 'fadeOutDown',
+            appendTo: 0,
             zindex: 1400
         });
     }
